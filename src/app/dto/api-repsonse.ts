1cb2cd329179316322e78356d3d09809d13d963b
darkheart101 <kurokokoro@windowslive.com>
2020-05-19 07:07:33 +0300

    api response dto

---
+++ src/app/dto/api-repsonse.ts
@@ -0,0 +1,5 @@
+export class ApiResponse
+{
+    message :string;
+    data : Array<any>;
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,8 +3,9 @@ import { AppConfig } from '../config/app-config';
 import { HttpClient } from '@angular/common/http';
 import { LoginPayload } from '../dto/login-payload';
 import { Observable } from 'rxjs';
-import {map} from 'rxjs/operators';
+import {map, catchError} from 'rxjs/operators';
 import {LocalStorageService} from 'ngx-webstorage';
+import { ApiResponse } from '../dto/api-repsonse';
 
 @Injectable({
   providedIn: 'root'
@@ -22,10 +23,12 @@ export class AuthService {
   login(loginPaylod: LoginPayload): Observable<boolean>
   {
     let full_login_url : string = this.appConfig.api_url + this.login_url;
-    return this.httpClient.post(full_login_url, loginPaylod).pipe(map(response=>{
-      this.localStorageService.store('authenticationToken', response.data.jwt);
-      this.localStorageService.store('username', response.data.username);
-      return true;
+    return this.httpClient.post<ApiResponse>(full_login_url, loginPaylod).pipe(
+      map(response=>{
+        if(response.data == null) return false;
+        this.localStorageService.store('authenticationToken', response.data.jwt);
+        this.localStorageService.store('username', response.data.username);
+        return true;
     }));
   }
 }
